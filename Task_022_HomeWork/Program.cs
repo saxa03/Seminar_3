a82a3284f3189e8f92680ef2c09eceb408a4acc6
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-06 16:56:38 +0800

    add Task 022 HomeWork

---
+++ Task_022_HomeWork/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число:");
+
+string number = Console.ReadLine();
+int N = number.Length;
+
+if (N == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} -> да");
+    }
+    else
+    {
+    Console.WriteLine($"{number} -> нет");
+    }
+}
+else
+{
+  Console.WriteLine($"{number} -> не является пятизначным");  
+}
