04ee7c12d8ec5a6520c234b417281b1c26cbec8d
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-06 17:36:08 +0800

    add Task 024 HomeWork

---
+++ Task_024_HomeWork/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+//
+//выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine());
+
+
+for (int k = 1; k <= N; k++)
+{
+    Console.Write($"{Math.Pow(k,3)},");
+}
